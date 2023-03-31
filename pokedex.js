11cd7f68626dabdf4f34db54a487d767027a9a90
Sator100 <82121955+sator100@users.noreply.github.com>
2023-03-30 21:17:52 -0500

    Add files via upload

---
+++ pokedex.js
@@ -0,0 +1,69 @@
+const ListaPokemon = document.querySelector("#ListaPokemon");
+const botonesHeader = document.querySelectorAll(" .btn-header");
+let URL = "https://pokeapi.co/api/v2/pokemon/";
+
+for(let i = 0; i<=251; i++){
+    fetch(URL + i)
+        .then((response) => response.json())
+        .then(data => mostrarPokemon(data))
+}
+
+function mostrarPokemon(poke){
+
+    let tipos = poke.types.map((type) => `<p class="${type.type.name} tipo">${type.type.name}</p>`)
+    tipos = tipos.join('');
+
+    let pokeId = poke.id.toString();
+    if(pokeId.length === 1){
+        pokeId = "00" + pokeId;
+    }else if(pokeId.length === 2){
+        pokeId = "0" + pokeId;
+    }
+
+
+    const div = document.createElement("div");
+    div.classList.add('pokemon');
+    div.innerHTML = `
+        <p class="pokemon-id-back">#${pokeId}</p>
+        <div class="imagen-pokemon">
+            <img src="${poke.sprites.other["official-artwork"].front_default}" alt="${poke.id}">
+        </div>
+        <div class="pokemon-info">
+            <div class="nombre-contenedor">
+                <p class="pokemon-id">#${pokeId}</p>
+                <h2 class="pokemon-nombre">${poke.name}</h2>
+            </div>
+            <div class="pokemon-tipos">
+                ${tipos}
+            </div>
+            <div class="pokemon-stats">
+                <p class="stat">${poke.height} dm</p>
+                <p class="stat">${poke.weight} Kg</p>
+            </div>
+        </div>
+    `;
+    ListaPokemon.append(div);
+}
+
+botonesHeader.forEach(boton => boton.addEventListener("click", (event) => {
+    const BotonId = event.currentTarget.id;
+
+    ListaPokemon.innerHTML = '';
+    
+    for(let i = 0; i<=251; i++){
+        fetch(URL + i)
+            .then((response) => response.json())
+            .then(data => {
+
+                if(BotonId === "ver-todos"){
+                    mostrarPokemon(data);
+                }else{
+                    const tipos = data.types.map((type) => type.type.name);
+                    if(tipos.some(tipo => tipo.includes(BotonId))){
+                        mostrarPokemon(data);
+                    }
+                }
+            })
+    }
+}
+));
